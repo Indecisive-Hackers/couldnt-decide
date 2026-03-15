d5f80c7dcaaea738744e83fc3b60c5d8b8841600
Harry Zoeftig <harry.zoeftig@btinternet.com>
2026-03-15 09:39:59 +0000

    added login component

---
+++ frontend/src/app/login/login.component.ts
@@ -0,0 +1,40 @@
+import { Component, inject } from '@angular/core';
+import { IUser, NewUser } from '../entities/user/user.model';
+import { FormsModule } from '@angular/forms';
+import { JsonPipe } from '@angular/common'
+import { UserAPI } from '../entities/user/user-api.service';
+import { finalize, Observable } from 'rxjs';
+import { HttpResponse } from '@angular/common/http';
+import { UserService } from '../services/user/user.service';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+  imports: [ FormsModule ]
+})
+export class LoginComponent {
+
+  model : NewUser = {username: "", password: "", id: null, email_address: ""}
+  submitted = false;
+  userAPI = inject(UserAPI);
+  userService = inject(UserService);
+
+  onSubmit() {
+    this.subscribeToSaveResponse(this.userAPI.loginUser(this.model));
+  }
+
+  private subscribeToSaveResponse(result : Observable<HttpResponse<IUser>>) : void {
+    result.pipe(finalize(() => this.submitted = true)).subscribe({
+      next: response => this.loginUser(response.body!),
+      error: () => alert("Please check your username or password."),
+    });
+  }
+
+  loginUser(response : any) : void {
+    localStorage.setItem('token', response['token']);
+    this.userService.setCurrentUser(response['user'] as IUser);
+  }
+
+}
--- frontend/src/app/services/user/user.service.ts
+++ frontend/src/app/services/user/user.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import {IUser} from "../../entities/user/user.model";
+import {BehaviorSubject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private userSave = new BehaviorSubject<null | IUser>(null);
 
   constructor() { }
+
+  setCurrentUser(user : IUser) {
+    this.userSave.next(user)
+  }
+  
+  clearCurrentUser() {
+    this.userSave.next(null)
+  }
 }
